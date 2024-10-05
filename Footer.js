de1d571208f9289c24ec8722ffad7e5d7ee9e105
AshithaSujith <ashithasujith8@gmail.com>
2024-10-05 11:11:38 +0530

    Create Footer.js

---
+++ Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        
+
+    <footer className="bg-light text-center py-4">
+        <div className="container">
+          <p className="mb-0">&copy; 2024 E-Shop. All rights reserved.</p>
+          <p>
+            <a href="#" className="text-primary">Privacy Policy</a> | 
+            <a href="#" className="text-primary"> Terms of Service</a>
+          </p>
+        </div>
+      </footer>
+    </div>
+  )
+}
