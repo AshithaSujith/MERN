4519bf9e77b7016c1b7b9bbae4c2132bc440c7b2
AshithaSujith <ashithasujith8@gmail.com>
2024-10-07 17:02:31 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import './App.css';
-import Front from './components/Front'; 
+import Front from './components/Front';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './components/Footer';
-import Backend from './components/Backend';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ProductDetail from './components/ProductDetail'; 
+// import BlogForm from './components/BlogForm';
 function App() {
   return (
-    <div className="App">
-      <Front/>
-     <Footer/>
-     <Backend/>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Front />} />
+          <Route path="/blog/:id" element={<ProductDetail />} />
+          {/* <Route path="/create-blog" element={<BlogForm />} /> */}
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
